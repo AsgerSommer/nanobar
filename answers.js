1074b8031b4c0a0baa81743e8d63c3bb4709399f
Asger Sommer <asger.sommer@gmail.com>
2017-11-08 16:50:46 +0100

    Updated

--- answers.js
+++ answers.js
@@ -1,18 +1,24 @@
 var date = new Date();
 var dato = date.getDay();
-var time = date.getHours()*100 + date.getMinutes()
+var time = date.getHours()*100 + date.getMinutes();
+var cleanDay = new Date(2017,10,08);
+var diff = date - cleanDay;
+var cleanDays = Math.floor(diff/(60*60*1000*24)*1);
 
-var yesWords = ["Ja", "Yeps", "Helt klart", "Altid", "Jepper", "Tjek", "True", "Yeah", "Jep", "Yes"];
-var noWords = ["Nej", "Næh", "Nope", "Aldrig", "Niksen biksen", "Næppe", "Helt klart ikke", "No", "Nah", "False"];
+var yesWords = ["Ja", "Yeps", "Helt klart", "Jepper", "Tjek", "True", "Yeah", "Jep", "Yes"];
+var noWords = ["Nej", "Næh", "Nope", "Niksen biksen", "Næppe", "Helt klart ikke", "No", "Nah", "False"];
 var catAnswers = ["Ja", "Nej"];
 
-var yesWord = yesWords[Math.floor(Math.random() * yesWords.length)];
+var yesWord1 = yesWords[Math.floor(Math.random() * yesWords.length)];
+var yesWord2 = yesWords[Math.floor(Math.random() * yesWords.length)];
 var noWord = noWords[Math.floor(Math.random() * noWords.length)];
 var catAnswer = catAnswers[Math.floor(Math.random() * catAnswers.length)];
 
-if (dato == 0 || dato == 6) {document.getElementById("svar1").innerHTML = yesWord + ", husk studiekort i weekenden.";}
-else if (1700 <= time || time <= 700) {document.getElementById("svar1").innerHTML = yesWord + ", husk studiekort efter kl. 17.";}
-else {document.getElementById("svar1").innerHTML = yesWord + ".";}
+if (dato == 0 || dato == 6) {document.getElementById("svar1").innerHTML = yesWord1 + ", husk studiekort i weekenden.";}
+else if (1700 <= time || time <= 700) {document.getElementById("svar1").innerHTML = yesWord1 + ", husk studiekort efter kl. 17.";}
+else {document.getElementById("svar1").innerHTML = yesWord1 + ".";}
 
-document.getElementById("svar2").innerHTML = ".." + noWord+".";
+// document.getElementById("svar2").innerHTML = ".." + noWord+".";
+if (cleanDays <= 180) {document.getElementById("svar2").innerHTML = yesWord2 + ", det er den faktisk!" ;}
+else {document.getElementById("svar2").innerHTML = noWord + ", men det var den for " + cleanDays + " dage siden." ;}
 document.getElementById("svar3").innerHTML = catAnswer+".";
